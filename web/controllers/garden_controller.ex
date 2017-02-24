1c6c32ed6515c9d1ab30213b7aa7587b9b4f831b
Dee Roberts <deerob4@gmail.com>
2017-02-24 00:02:15 +0000

    Prevent params exceeding limits

--- web/controllers/garden_controller.ex
+++ web/controllers/garden_controller.ex
@@ -5,9 +5,28 @@ defmodule Gardener.GardenController do
     render(conn, "index.html")
   end
 
+  def generate(conn, %{"goal" => goal}) when byte_size(goal) > 50 do
+    conn
+    |> put_status(:bad_request)
+    |> render("goal_too_big.json")
+  end
+
   def generate(conn, %{"goal" => goal} = params) do
-    population = params |> Map.get("population", "1000") |> String.to_integer()
-    plant = Gardener.Speller.spell(goal, population_size: population)
-    json(conn, plant)
+    population = get_population(params)
+
+    if population > 10000 do
+      conn
+      |> put_status(:bad_request)
+      |> render("population_too_big.json")
+    else
+      plant = Gardener.Speller.spell(goal, population_size: population)
+      render(conn, "result.json", population: plant)
+    end
+  end
+
+  defp get_population(params) do
+    params
+    |> Map.get("population", "1000")
+    |> String.to_integer()
   end
 end
--- web/views/garden_view.ex
+++ web/views/garden_view.ex
@@ -1,3 +1,15 @@
 defmodule Gardener.GardenView do
   use Gardener.Web, :view
+
+  def render("result.json", %{population: population}) do
+    population
+  end
+
+  def render("goal_too_big.json", _params) do
+    %{error: "Goal cannot exceed 50 characters"}
+  end
+
+  def render("population_too_big.json", _params) do
+    %{error: "Population cannot exceed 10000"}
+  end
 end
